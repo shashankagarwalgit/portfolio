5120bcdc4b6ba18942280a09eb0e59b8cd29b025
ershashankgit <112311687+ershashankgit@users.noreply.github.com>
2024-03-07 14:39:44 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import Navbar from "./components/Navbar/index";
+import Skills from "./components/Skills/index";
+import Hero from "./components/HeroSection/index";
+import Education from "./components/Education/index";
+import { darkTheme } from "./utils/Themes";
+
+const Body = styled.div`
+background-color: ${({ theme }) => theme.bg}; 
+width: 100%;
+height: 100%
+overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+      <Body>
+        <Hero />
+        <Wrapper>
+        <Skills />
+        <Education />
+        </Wrapper>
+      </Body>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,16 @@
+export const Bio = {
+    name: "Shashank Agarwal",
+    roles: [
+      "Full Stack Developer",
+    ],
+    description:
+      "I am a Student Pursuing Chemical Engineering from ZHCET, AMU in Aligarh. I am a hardworking student seeking opportunities to express and elevate my skills in various fields. Bringing forth a positive attitude and the willingness and motivation to learn new things.",
+    github: "https://github.com/ershashankgit",
+    resume:
+      "https://storageapi.shashank.my.id/data/Shashank_Agarwal_CV.pdf",
+    linkedin: "https://www.linkedin.com/in/shashankagarwalme/",
+    twitter: "https://twitter.com/shashank_my_id",
+    insta: "https://www.instagram.com/shashank.agarwal.me/",
+    facebook: "https://www.facebook.com/shashank.my.id/",
+  };
+  
